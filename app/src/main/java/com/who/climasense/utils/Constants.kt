39d3fd9f4d037647c245b7b872534ef9dd598faf
dotnetbabu <ayush06092002@gmail.com>
2024-04-09 22:48:00 +0530

    Update Constants.kt

--- app/src/main/java/com/who/climasense/utils/Constants.kt
+++ app/src/main/java/com/who/climasense/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.who.climasense.utils
 
 object Constants {
     val BASE_URL = "https://api.openweathermap.org/"
-    val API_KEY = "5492068ad394673828bd00981f6d8fca"
+    val API_KEY = "" //Paste Your API Key here
 }
